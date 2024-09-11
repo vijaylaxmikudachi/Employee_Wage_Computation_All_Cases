0a585af2e2efcb6d9dce9d906a23b305dc75ffbe
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-11 10:08:20 +0530

    Attendence

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,5 +1,15 @@
-public class EmpWageComputation {
+import java.util.Random;
+
+public class Emp_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        // UC-1: Employee Attendance
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
