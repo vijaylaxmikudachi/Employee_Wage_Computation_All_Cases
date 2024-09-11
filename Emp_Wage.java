02a20920966b6bd74a62a82c00942500b8f8a154
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-11 10:12:08 +0530

    Part time  wages

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,22 +1,33 @@
 import java.util.Random;
 
 public class Emp_Wage {
+    // Constants
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
-        // UC-1: Employee Attendance
+        // Randomly determine whether the employee is full-time or part-time
         Random random = new Random();
-        int attendance = random.nextInt(2);
-        if (attendance == 1) {
-            System.out.println("Employee is Present");
-            // UC-2: Calculate Daily Wage
-            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-            System.out.println("Daily Wage of Employee: " + dailyWage);
+        int employeeType = random.nextInt(2); // 0 for part-time, 1 for full-time
+
+        int workingHours;
+        if (employeeType == 1) {
+            // Full-time employee
+            workingHours = FULL_DAY_HOURS;
+            System.out.println("Employee is Full-Time");
         } else {
-            System.out.println("Employee is Absent");
+            // Part-time employee
+            workingHours = PART_TIME_HOURS;
+            System.out.println("Employee is Part-Time");
         }
+
+        // Calculate the wage
+        int dailyWage = WAGE_PER_HOUR * workingHours;
+
+        // Display the calculated daily wage
+        System.out.println("Daily Employee Wage: $" + dailyWage);
     }
 }
