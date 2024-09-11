c11c38a96164cb95d9ef1914aa8ed5d2a1a81023
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-11 10:13:58 +0530

    Switch case Part time  wages

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,27 +1,36 @@
 import java.util.Random;
 
 public class Emp_Wage {
+
     // Constants
     public static final int WAGE_PER_HOUR = 20;
-    public static final int FULL_DAY_HOURS = 8;
-    public static final int PART_TIME_HOURS = 4;
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
-        // Randomly determine whether the employee is full-time or part-time
+        // Randomly determine whether the employee is full-time, part-time, or absent
         Random random = new Random();
-        int employeeType = random.nextInt(2); // 0 for part-time, 1 for full-time
+        int employeeType = random.nextInt(3); // 0 for absent, 1 for full-time, 2 for part-time
 
-        int workingHours;
-        if (employeeType == 1) {
-            // Full-time employee
-            workingHours = FULL_DAY_HOURS;
-            System.out.println("Employee is Full-Time");
-        } else {
-            // Part-time employee
-            workingHours = PART_TIME_HOURS;
-            System.out.println("Employee is Part-Time");
+        int workingHours = 0;
+
+        // Switch case to handle employee types
+        switch (employeeType) {
+            case FULL_TIME:
+                workingHours = 8;  // Full-time employee works 8 hours
+                System.out.println("Employee is Full-Time");
+                break;
+
+            case PART_TIME:
+                workingHours = 4;  // Part-time employee works 4 hours
+                System.out.println("Employee is Part-Time");
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
 
         // Calculate the wage
