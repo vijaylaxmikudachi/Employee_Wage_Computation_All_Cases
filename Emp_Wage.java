08ec2f3e0fea7f3ec6a921b18baf6ea4bde54e12
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-11 10:10:10 +0530

    Daily wages

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,13 +1,20 @@
 import java.util.Random;
 
 public class Emp_Wage {
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOURS = 8;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
         // UC-1: Employee Attendance
         Random random = new Random();
         int attendance = random.nextInt(2);
         if (attendance == 1) {
             System.out.println("Employee is Present");
+            // UC-2: Calculate Daily Wage
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+            System.out.println("Daily Wage of Employee: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
         }
